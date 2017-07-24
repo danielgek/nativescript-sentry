89268a7bab48ef7e94bb22c94d5d893bdb727816
Daniel Leal <danielgek@gmail.com>
2017-07-24 11:37:05 +0100

    Clean angular demo

--- demo-ng/app/app.module.ts
+++ demo-ng/app/app.module.ts
@@ -1,21 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
-
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-
-// Uncomment and add to NgModule imports if you need to use two-way binding
-// import { NativeScriptFormsModule } from "nativescript-angular/forms";
-
-// Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
-// import { NativeScriptHttpModule } from "nativescript-angular/http";
-
 import { SentryModule } from 'nativescript-sentry/angular';
-
-
 
 @NgModule({
     bootstrap: [
@@ -28,17 +14,11 @@ import { SentryModule } from 'nativescript-sentry/angular';
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
     ],
-    providers: [
-        ItemService
-    ],
+    providers: [ ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
 })
-/*
-Pass your application module to the bootstrapModule function located in main.ts to start your app
-*/
+
 export class AppModule { }
