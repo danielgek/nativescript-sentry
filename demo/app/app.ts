7ad2610dfa21959c914d9459afc33cb9f75004e9
Murat Çorlu <murat.corlu@philips.com>
2017-08-27 13:07:27 +0200

    dns -> dsn typo fix

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,6 +1,6 @@
 ﻿import * as application from 'tns-core-modules/application';
 import { Sentry } from 'nativescript-sentry';
-const dns = 'https://6073dd1015a344ce9028507b5c7abd34:4770c1cecb904a568c85f1682c87fc83@watson.mysns.pt/3';
-Sentry.init(dns);
+const dsn = 'https://6073dd1015a344ce9028507b5c7abd34:4770c1cecb904a568c85f1682c87fc83@watson.mysns.pt/3';
+Sentry.init(dsn);
 application.start({ moduleName: "main-page" });
 
