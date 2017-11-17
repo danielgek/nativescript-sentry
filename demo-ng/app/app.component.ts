9620da50623cfacbcba72c69e5f75b72a6473e1c
Daniel Leal <danielgek@gmail.com>
2017-11-17 11:39:32 +0000

    chore: update page tests

--- demo-ng/app/app.component.ts
+++ demo-ng/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
-
+import { OnInit } from "@angular/core";
+import { Sentry } from "nativescript-sentry";
+import * as utils from 'utils/utils';
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html",
@@ -8,14 +10,21 @@ import { Component } from "@angular/core";
 export class AppComponent { 
     onTapTry(eventData) {
         try {
-            throw 'try catch exeption example'
+            throw 'try catch exeption example';
         } catch (error) {
-            console.log('Exeption')
-            console.log(error);
+            Sentry.capture(error);
+        }
+    }
+
+    onTapTryError(eventData) {
+        try {
+            throw new Error('try catch exeption example');
+        } catch (error) {
+            Sentry.capture(error);
         }
     }
 
     onTapMain(eventData) {
-        throw 'Test Sentry on Main thread'
+        throw 'Test Sentry on Main thread';
     }
 }
