04408fe21ede4c5eba41e4ae333630fae7e59882
Brad Martin <bradwaynemartin@gmail.com>
2018-07-22 23:10:34 -0500

    clean up

--- demo-ng/app/app.component.ts
+++ demo-ng/app/app.component.ts
@@ -1,83 +1,84 @@
-import { Component } from "@angular/core";
-import { OnInit } from "@angular/core";
-import { Sentry, SentryBreadcrumb, SentrySeverity } from "nativescript-sentry";
-import * as utils from 'utils/utils';
+import { Component, OnInit } from '@angular/core';
+import { Sentry, SentryBreadcrumb } from 'nativescript-sentry';
+
 @Component({
-    selector: 'ns-app',
-    templateUrl: 'app.component.html'
+  selector: 'ns-app',
+  templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit {
-    ngOnInit() {
-        // Sentry.onBeforeSend((event) => {
-        // event.tags = NSDictionary.alloc<string, string>().initWithObjectsForKeys(
-        //     utils.ios.collections.jsArrayToNSArray(['true', 'true']) as NSArray<string>,
-        //     utils.ios.collections.jsArrayToNSArray(['tag1', 'tag2']) as NSArray<string>
-        // );
-        // console.log('passei no componente antes de enviar o evento');
-        // setTimeout(() => {
-        //     console.log('vou returnar true no component');
-        //     return true;
-        // }, 3000);
-        // });
-        Sentry.setContextUser({
-            email: 'daniel@fnaile.com',
-            username: 'danielgek'
-        });
-        Sentry.setContextTags({
-            tag1: 'value1',
-            tag2: 'value2'
-        });
-        Sentry.setContextExtra({
-            extra1: 'valuextra1',
-            extra2: 'valuextra2'
-        });
+  ngOnInit() {
+    // Sentry.onBeforeSend((event) => {
+    // event.tags = NSDictionary.alloc<string, string>().initWithObjectsForKeys(
+    //     utils.ios.collections.jsArrayToNSArray(['true', 'true']) as NSArray<string>,
+    //     utils.ios.collections.jsArrayToNSArray(['tag1', 'tag2']) as NSArray<string>
+    // );
+    // console.log('passei no componente antes de enviar o evento');
+    // setTimeout(() => {
+    //     console.log('vou returnar true no component');
+    //     return true;
+    // }, 3000);
+    // });
+    Sentry.setContextUser({
+      email: 'daniel@fnaile.com',
+      username: 'danielgek'
+    });
+    Sentry.setContextTags({
+      tag1: 'value1',
+      tag2: 'value2'
+    });
+    Sentry.setContextExtra({
+      extra1: 'valuextra1',
+      extra2: 'valuextra2'
+    });
+  }
+
+  onTapTry(eventData) {
+    try {
+      throw 'try catch exeption example';
+    } catch (error) {
+      Sentry.captureException(error, {});
     }
-    onTapTry(eventData) {
-        try {
-            throw 'try catch exeption example';
-        } catch (error) {
-            Sentry.captureException(error, {});
+  }
+
+  onTapTryError(eventData) {
+    try {
+      throw new Error('try catch exeption example');
+    } catch (error) {
+      Sentry.captureException(error, {});
+    }
+  }
+
+  onTapNative(eventData) {
+    throw 'Test Sentry on Main thread';
+  }
+
+  onTapMessage() {
+    Sentry.captureMessage('bazinga, you got a message', {});
+  }
+
+  onTapBreadcrumb() {
+    const breadcrumb: SentryBreadcrumb = {
+      message: 'bazinga, you got a breadcrumb message',
+      category: 'breadcrumb category',
+      data: {
+        custom: 'value'
+      }
+    };
+    Sentry.captureBreadcrumb(breadcrumb);
+  }
+
+  onTapCaptureWithExtras() {
+    try {
+      throw new Error('try catch exeption example');
+    } catch (error) {
+      Sentry.captureException(error, {
+        tags: {
+          tagevent: 'tageventvalue'
+        },
+        extra: {
+          extraevent: 'extraeventvalue'
         }
+      });
     }
-
-    onTapTryError(eventData) {
-        try {
-            throw new Error('try catch exeption example');
-        } catch (error) {
-            Sentry.captureException(error, {});
-        }
-    }
-
-    onTapNative(eventData) {
-        throw 'Test Sentry on Main thread';
-    }
-
-    onTapMessage() {
-        Sentry.captureMessage('bazinga, you got a message', {});
-    }
-    onTapBreadcrumb() {
-        const breadcrumb: SentryBreadcrumb = {
-            message: 'bazinga, you got a breadcrumb message',
-            category: 'breadcrumb category',
-            data: {
-                custom: 'value'
-            }
-        };
-        Sentry.captureBreadcrumb(breadcrumb);
-    }
-
-    onTapCaptureWithExtras() {
-        try {
-            throw new Error('try catch exeption example');
-        } catch (error) {
-            Sentry.captureException(error, {
-                tags: {
-                    tagevent: 'tageventvalue'
-                },
-                extra: {
-                    extraevent: 'extraeventvalue'
-                }
-            });
-        }
-    }
+  }
 }
--- demo-ng/app/app.module.ts
+++ demo-ng/app/app.module.ts
@@ -1,22 +1,18 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { AppComponent } from "./app.component";
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { SentryModule } from 'nativescript-sentry/angular';
+import { AppComponent } from './app.component';
 
 @NgModule({
-    bootstrap: [
-        AppComponent
-    ],
-    imports: [
-        NativeScriptModule,
-        SentryModule.forRoot({ dsn: 'https://58d98e428d6746998c1898bdf129533a:5a2b4c3bfdbd4656b8777b1b271343a9@watson.mysns.pt/4'})
-    ],
-    declarations: [
-        AppComponent,
-    ],
-    providers: [ ],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+  bootstrap: [AppComponent],
+  imports: [
+    NativeScriptModule,
+    SentryModule.forRoot({
+      dsn: 'https://58d98e428d6746998c1898bdf129533a:5a2b4c3bfdbd4656b8777b1b271343a9@watson.mysns.pt/4'
+    })
+  ],
+  declarations: [AppComponent],
+  providers: [],
+  schemas: [NO_ERRORS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {}
