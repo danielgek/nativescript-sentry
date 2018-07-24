8536e4030a2eadde9ffdb4c54d70000186ea728a
Brad Martin <bradwaynemartin@gmail.com>
2018-07-24 09:53:12 -0500

    go

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,7 +1,6 @@
 ﻿import { Sentry } from 'nativescript-sentry';
 import * as application from 'tns-core-modules/application';
-// const dsn = 'https://6073dd1015a344ce9028507b5c7abd34:4770c1cecb904a568c85f1682c87fc83@watson.mysns.pt/3';
-const sentryDsn = 'https://aaa25eb556fa476a92e0edea6dd57af6:65c984b9260e47f0bb128def7eddd5f4@sentry.io/306438';
+const dsn = 'https://6073dd1015a344ce9028507b5c7abd34:4770c1cecb904a568c85f1682c87fc83@watson.mysns.pt/3';
 
-Sentry.init(sentryDsn);
+Sentry.init(dsn);
 application.start({ moduleName: 'main-page' });
